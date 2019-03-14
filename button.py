210aa8382978a75939a54b1f1111ec5a67a99bfc
gor_lobs <nikita.lobosov.wa@yandex.ru>
2019-03-14 19:03:20 +0300

    Add dec and inc buttons

--- button.py
+++ button.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("250x200")
+root.geometry("250x315")
 root.title("Кнопки")
 root.configure(background="#003344")
 
@@ -14,9 +14,29 @@ def show(event):
     num = 111
     show_label['text'] = num
 
+show_button.bind('<Button-1>', show)
+
+inc_button = tk.Button(root, text="Увеличить", width=12, height=3)
+inc_button.config(font=("Courier", 15))
+
+def inc(event):
+    num = show_label['text']
+    show_label['text'] = num + 1
+
+inc_button.bind('<Button-1>', inc)
+
+dec_button = tk.Button(root, text="Уменьшить", width=12, height=3)
+dec_button.config(font=("Courier", 15))
+
+def вус(event):
+    num = show_label['text']
+    show_label['text'] = num - 1
+
+dec_button.bind('<Button-1>', вус)
 
 show_label.pack()
 show_button.pack()
-show_button.bind('<Button-1>', show)
+inc_button.pack()
+dec_button.pack()
 
 root.mainloop()
